d6be2890c6ace9d95ed8a3c4c1ec5793db222464
Gidon Tobin <gidontobin@gmail.com>
2021-10-17 15:40:13 +0300

    finished tasks without testing

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -8,39 +8,66 @@
 #include "anomaly_detection_util.h"
 
 float avg(float* x, int size){
-	return 0;
+	float sum =0;
+	for (int i=0;i<size;i++){
+	    sum = sum + x[i];
+	}
+    return sum/size;
 }
 
-// returns the variance of X and Y
+// returns the variance of X
 float var(float* x, int size){
-	return 0;
+	float avg = avg(x,size);
+    float sum = 0;
+    for (int i=0;i<size;i++){
+        sum = sum +powf(x[i]-avg,2);
+    }
+    return sum/size;
+
 }
 
 // returns the covariance of X and Y
 float cov(float* x, float* y, int size){
-	return 0;
+    float avX = avg(x,size);
+    float avY = avg(y,size);
+    float sum = 0;
+    for (int i=0;i<size;i++){
+        sum = sum + x[i]*y[i];
+    }
+    sum = sum/size;
+    return sum - avX*avY;
 }
 
 
 // returns the Pearson correlation coefficient of X and Y
 float pearson(float* x, float* y, int size){
-	return 0;
+	float sqX=sqrtf(var(x,size));
+    float sqY=sqrtf(var(y,size));
+    return cov(x,y,size)/(sqX*sqY);
 }
 
 // performs a linear regression and returns the line equation
 Line linear_reg(Point** points, int size){
-
-	return Line(0,0);
+    float x[size];
+    float y[size];
+    for (int i=0;i<size;i++){
+        x[i]=points[i].x;
+        y[i] = points[i].y;
+    }
+    float a = cov(x,y,size)/var(x,size);
+    float b = avg(y,size)-(a*avg(x,size));
+	return Line(a,b);
 }
 
 // returns the deviation between point p and the line equation of the points
 float dev(Point p,Point** points, int size){
-	return 0;
+	Line reg = linear_reg(points,size);
+	return reg.a*p.x+reg.b;
 }
 
 // returns the deviation between point p and the line
 float dev(Point p,Line l){
-	return 0;
+    return l.a*p.x+l.b;
 }
 
 
